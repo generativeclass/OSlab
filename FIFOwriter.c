32e304aa388b939d197802f5b1ad7c04aac75b79
generativeclass <145311557+generativeclass@users.noreply.github.com>
2024-12-06 22:49:28 +0530

    Update FIFOwriter.c

--- FIFOwriter.c
+++ FIFOwriter.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define FIFO_FILE "/tmp/myfifo"
-
 int main() {
-    char message[100];
     int fd;
-
-    // Create the FIFO if it doesn't exist
-    if (mkfifo(FIFO_FILE, 0666) == -1) {
-        perror("mkfifo");
+    char *myfifo = "/tmp/myfifo";
+    
+    // Create the FIFO (named pipe)
+    mkfifo(myfifo, 0666);
+    
+    char arr1[80], arr2[80];
+    
+    while (1) {
+        // Open FIFO for writing
+        fd = open(myfifo, O_WRONLY);
+        
+        // Input message
+        printf("You: ");
+        fgets(arr2, 80, stdin);
+        
+        // Write the message to the FIFO
+        write(fd, arr2, strlen(arr2) + 1);
+        close(fd);
+        
+        // Open FIFO for reading response
+        fd = open(myfifo, O_RDONLY);
+        
+        // Read response from user2
+        read(fd, arr1, sizeof(arr1));
+        printf("User2: %s\n", arr1);
+        close(fd);
     }
 
-    // Open FIFO for writing
-    fd = open(FIFO_FILE, O_WRONLY);
-    if (fd == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-
-    // Get a message to write to the FIFO
-    printf("Enter message to send: ");
-    fgets(message, sizeof(message), stdin);
-
-    // Write message to FIFO
-    write(fd, message, strlen(message) + 1);
-    
-    // Close the FIFO
-    close(fd);
-    
     return 0;
 }
